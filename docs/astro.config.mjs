208d24e02cd552bdb1a4e81d382d03022358b25a
Raúl Castilla Bravo <raulcastillabravo@gmail.com>
2026-05-13 21:09:27 +0200

    feat: Add GitHub Action to deploy the Starlight web

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://raulcastillabravo.github.io',
+	base: '/mve-collection/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
