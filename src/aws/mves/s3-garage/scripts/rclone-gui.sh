5a3782f529ab546b77f11968e4b28a49d5db6a23
Raúl Castilla Bravo <75526935+raulcastillabravo@users.noreply.github.com>
2026-06-17 09:47:13 +0200

    fix: Manual infra steps in docs and rclone-gui for non dev container setup (#28)
    * fix: Fix manual infra steps in docs and rclone-gui for non dev container setup
    * fix: GitHub Actions must run in fix branches

--- src/aws/mves/s3-garage/scripts/rclone-gui.sh
+++ src/aws/mves/s3-garage/scripts/rclone-gui.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rclone --config="config/rclone.conf" rcd --rc-web-gui --rc-no-auth --rc-web-gui-no-open-browser
+mise exec -- rclone --config="config/rclone.conf" rcd --rc-web-gui --rc-no-auth --rc-web-gui-no-open-browser
